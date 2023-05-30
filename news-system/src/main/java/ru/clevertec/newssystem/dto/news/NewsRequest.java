fa6cb2eb7b951fcf235e891cc7f0f2a5daf96db6
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-05-30 19:21:58 +0300

    feat: add NewsService integration test

--- news-system/src/main/java/ru/clevertec/newssystem/dto/news/NewsRequest.java
+++ news-system/src/main/java/ru/clevertec/newssystem/dto/news/NewsRequest.java
@@ -1,10 +1,18 @@
 package ru.clevertec.newssystem.dto.news;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewsRequest {
 
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String text;
 }
