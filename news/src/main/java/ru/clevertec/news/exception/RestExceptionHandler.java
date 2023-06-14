1538fcd27095b63ac48fa9207a4511fc81339bd8
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-06-14 13:25:08 +0300

    feat: configure logback

--- news/src/main/java/ru/clevertec/news/exception/RestExceptionHandler.java
+++ news/src/main/java/ru/clevertec/news/exception/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package ru.clevertec.news.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,6 +11,7 @@ import ru.clevertec.news.dto.ErrorEntity;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class RestExceptionHandler {
 
@@ -19,6 +21,7 @@ public class RestExceptionHandler {
         String code = String.valueOf(status);
         String message = exception.getMessage();
         ErrorEntity error = new ErrorEntity(code, message);
+        log.error("Caught RuntimeException with message: {}", message);
         return ResponseEntity
                 .status(status)
                 .body(error);
@@ -30,6 +33,7 @@ public class RestExceptionHandler {
         String code = status + String.valueOf(exception.getId());
         String message = exception.getMessage() + "(id=" + exception.getId() + ").";
         ErrorEntity error = new ErrorEntity(code, message);
+        log.warn("Caught EntityNotFoundException with message: {}", message);
         return ResponseEntity
                 .status(status)
                 .body(error);
@@ -46,6 +50,7 @@ public class RestExceptionHandler {
                     String errorMessage = error.getDefaultMessage();
                     errors.put(fieldName, errorMessage);
                 });
+        log.warn("Caught MethodArgumentNotValidException with message: {}", ex.getMessage());
         return ResponseEntity
                 .status(status)
                 .body(errors);
