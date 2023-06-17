07a4c8eb32b4f21f63d6d88b27cf707f4145e24e
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-06-17 11:32:46 +0300

    refactor: change names

--- user-service/src/main/java/ru/clevertec/user/UserServiceApplication.java
+++ user-service/src/main/java/ru/clevertec/user/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package ru.clevertec.users;
+package ru.clevertec.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- user-service/src/test/java/ru/clevertec/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/ru/clevertec/user/UserServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.clevertec.users;
+package ru.clevertec.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
