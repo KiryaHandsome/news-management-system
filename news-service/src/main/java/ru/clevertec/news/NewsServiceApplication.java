b97b69924c13a626c2e33f8c372b378d0bed9de9
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-06-28 17:58:32 +0300

    feat: add cloud config server and move property files to it

--- news-service/src/main/java/ru/clevertec/news/NewsServiceApplication.java
+++ news-service/src/main/java/ru/clevertec/news/NewsServiceApplication.java
@@ -3,10 +3,12 @@ package ru.clevertec.news;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableCaching
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NewsServiceApplication {
 
