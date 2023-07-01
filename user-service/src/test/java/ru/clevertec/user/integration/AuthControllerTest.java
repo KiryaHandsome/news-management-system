849920aa16e45b649b420817385ee67b8561e3b1
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-07-02 02:44:39 +0300

    build: modify build.gradle to build all modules from the root

--- user-service/src/test/java/ru/clevertec/user/integration/AuthControllerTest.java
+++ user-service/src/test/java/ru/clevertec/user/integration/AuthControllerTest.java
@@ -34,7 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
-class AuthControllerTest extends BaseIntegrationTest {
+class AuthControllerTest {
 
     private ObjectMapper objectMapper;
 
